2ad9056ebc5ef6664ca8b85bfbbd23ea917c8f01
pc-work <adipol13@gmail.com>
2023-06-01 16:49:41 -0400

    se coloco mix a los js

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -17,7 +17,7 @@
     @livewireStyles
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </head>
 
--- resources/views/layouts/import.blade.php
+++ resources/views/layouts/import.blade.php
@@ -17,7 +17,7 @@
     @livewireStyles
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 
 <body class="font-sans antialiased">
