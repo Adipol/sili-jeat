6e3fee881f0e78bf6c352373864a3c72b84dcfe5
work-pc <adipol13@gmail.com>
2025-04-22 15:19:03 -0400

    base datos ex pep

---
+++ database/migrations/2025_04_22_115840_add_ex_peps_upgrade_to_imports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddExPepsUpgradeToImportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('imports', function (Blueprint $table) {
+            $table->integer('ex_peps_upgrade')->nullable()->after('canadienses_upgrade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('imports', function (Blueprint $table) {
+            $table->dropColumn('ex_peps_upgrade');
+        });
+    }
+}
