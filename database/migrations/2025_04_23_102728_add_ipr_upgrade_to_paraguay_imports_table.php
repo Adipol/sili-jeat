4601b853821f9ad40f9433ab62affcd776ef5260
work-pc <adipol13@gmail.com>
2025-04-23 10:30:30 -0400

    base de datos ipr

---
+++ database/migrations/2025_04_23_102728_add_ipr_upgrade_to_paraguay_imports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIprUpgradeToParaguayImportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('paraguay_imports', function (Blueprint $table) {
+            $table->integer('ipr_upgrade')->nullable()->after('ex_peps_upgrade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('paraguay_imports', function (Blueprint $table) {
+            $table->dropColumn('ipr_upgrade');
+        });
+    }
+}
