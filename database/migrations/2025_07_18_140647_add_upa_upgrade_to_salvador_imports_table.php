0b0c838c8a907911d4de79da919933465ee270e3
work-pc <adipol13@gmail.com>
2025-07-18 14:48:49 -0400

    base de datos upa

---
+++ database/migrations/2025_07_18_140647_add_upa_upgrade_to_salvador_imports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUpaUpgradeToSalvadorImportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('salvador_imports', function (Blueprint $table) {
+            $table->integer('upa_upgrade')->nullable()->after('dea_upgrade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('salvador_imports', function (Blueprint $table) {
+            $table->dropColumn('upa_upgrade');
+        });
+    }
+}
---
+++ database/migrations/2025_07_18_142420_add_upa_new_to_guatemala_imports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUpaNewToGuatemalaImportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('guatemala_imports', function (Blueprint $table) {
+            $table->integer('upa_new')->nullable()->after('dea_new');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('guatemala_imports', function (Blueprint $table) {
+            $table->dropColumn('upa_new');
+        });
+    }
+}
